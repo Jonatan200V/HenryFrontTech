b8b8a727aef1f6c8f20f60a6101c35aa3240abf7
Jonatan <jonatanangel88@gmail.com>
2022-12-12 16:18:54 -0300

    master-dep

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -31,7 +31,7 @@ export const MESSAGE_ADMIN = '@message/message-admin';
 export const USER_ALL_MSG = '@message/user-all-msg';
 export const MESSAGE_USER = '@message/user-all-admin';
 export const CHANGE_USER = '@message/change-user';
-export const api = 'https://henry-back.vercel.app';
+export const api = 'https://backend-tech.vercel.app';
 // export const api = 'http://localhost:3001';
 //ORDER
 export function orderSelect(order) {
